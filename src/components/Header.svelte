880cb2c96f8ff291b00fd03107a33d95e1e87c4e
aroyer1 <arno.royer2004@gmail.com>
2024-03-26 18:21:49 +0100

    Ajout du component header

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<header>
+    <nav>
+        <h3>ESCAPE.</h3>
+        <div class="nav">
+            <a href="#"><span class="nav-elt">Carte</span></a>
+            <a href="#"><span class="nav-elt">Modèle</span></a>
+        </div>
+    </nav>
+</header>
+
+<style>
+    nav {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px 30px;
+    }
+
+    h3 {
+        font-weight: 700;
+        color: #617285;
+        font-size: 1.4em;
+    }
+
+    .nav-elt {
+        margin: 0 10px;
+        font-size: 1.1em;
+        cursor: pointer;
+        color: #617285;
+    }
+</style>
