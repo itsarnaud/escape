cf7db11e40b5d90d64f324f069f1fdbdbc063c72
aroyer1 <arno.royer2004@gmail.com>
2024-03-29 03:00:38 +0100

    Ajout du model 3d avec threeJS

--- src/routes/modele/+page.svelte
+++ src/routes/modele/+page.svelte
@@ -1,14 +1,60 @@
 <script>
-    import Footer from '../../components/Footer.svelte';
+    import { onMount } from 'svelte';
+    import * as THREE from 'three';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
     import Header from '../../components/Header.svelte';
+    import Footer from '../../components/Footer.svelte';
+
+    let container;
+
+    onMount(async () => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const light = new THREE.AmbientLight(0xffffff); // lumière blanche
+        scene.add(light);   
+        const renderer = new THREE.WebGLRenderer();
+        let obj;
+
+        renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(renderer.domElement);
+
+        const loader = new GLTFLoader();
+        loader.load('/landskip.glb', function(gltf) {
+            console.log("Ça marche !!");
+            obj = gltf.scene;
+            scene.add( obj );
+        }, undefined, function ( error ) {
+            console.error( error );
+        } );
+
+        camera.position.z = 70;
+        camera.position.y = 20;
+
+        const animate = function() {
+            requestAnimationFrame(animate);
+            if (obj) {
+                obj.rotation.y += 0.01; // changez cette valeur pour ajuster la vitesse de rotation
+            }
+            renderer.render(scene, camera);
+        };
+
+        window.addEventListener('resize', function() {
+            renderer.setSize(container.clientWidth, container.clientHeight);
+            camera.aspect = container.clientWidth / container.clientHeight;
+            camera.updateProjectionMatrix();
+        });
+
+        animate();
+    });
 </script>
 
 <Header />
-
 <div class="container">
-    <iframe title="Coffre" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/3cfc78911c104450bde5212518b5d2aa/embed"> </iframe>
+    <div class="iframe" bind:this={container}></div>
     <h3>Le Langskip</h3>
 </div>
+
+
 
 <div class="article">
     <p>Lorem ipsum dolor sit amet consectetur. Pulvinar urna viverra interdum pharetra. Magnis tincidunt ut accumsan enim malesuada volutpat metus. Scelerisque in arcu purus volutpat ipsum sit. Bibendum porta amet viverra suspendisse arcu. Ultricies malesuada arcu tortor tincidunt. Nisi justo velit nunc aliquam proin scelerisque id id. Vel pretium enim metus eleifend amet. Vitae ac cursus aliquet faucibus vel nibh sem ligula pellentesque. Sem nec gravida id risus id massa scelerisque est. Tempor ut sodales sit tristique condimentum id magna. Non sed turpis arcu viverra turpis aliquet in vitae tortor.</p>
@@ -28,7 +74,7 @@
         gap: 10px;
     }
 
-    iframe {
+    .iframe {
         width: 256px;
         height: 144px;
     }
@@ -50,14 +96,14 @@
             border-radius: 1.4em;
         }
 
-        iframe {
+        .iframe {
             width: 426px;
             height: 240px;
         }
     }
 
     @media (min-width: 1024px) {
-        iframe {
+        .iframe {
             width: 800px;
             height: 450px;
         }
@@ -73,7 +119,7 @@
     }
 
     @media (min-width: 1440px) {
-        iframe {
+        .iframe {
             width: 960px;
             height: 540px;
         }
