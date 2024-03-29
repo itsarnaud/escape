765297544bd1a5188e7346b23db922e209ee5b30
aroyer1 <arno.royer2004@gmail.com>
2024-03-29 13:57:55 +0100

    Ajout de l'article

--- src/routes/modele/+page.svelte
+++ src/routes/modele/+page.svelte
@@ -19,6 +19,7 @@
         container.appendChild(renderer.domElement);
 
         const loader = new GLTFLoader();
+        console.log('ça charge...');
         loader.load('https://langskip.s3.eu-west-3.amazonaws.com/landskip.glb', function(gltf) {
             console.log("Ça marche !!");
             obj = gltf.scene;
@@ -57,7 +58,13 @@
 
 
 <div class="article">
-    <p>Lorem ipsum dolor sit amet consectetur. Pulvinar urna viverra interdum pharetra. Magnis tincidunt ut accumsan enim malesuada volutpat metus. Scelerisque in arcu purus volutpat ipsum sit. Bibendum porta amet viverra suspendisse arcu. Ultricies malesuada arcu tortor tincidunt. Nisi justo velit nunc aliquam proin scelerisque id id. Vel pretium enim metus eleifend amet. Vitae ac cursus aliquet faucibus vel nibh sem ligula pellentesque. Sem nec gravida id risus id massa scelerisque est. Tempor ut sodales sit tristique condimentum id magna. Non sed turpis arcu viverra turpis aliquet in vitae tortor.</p>
+    <h4>Le Langskip : Symbole de puissance et d'ingéniosité nordique</h4>
+    <p>Dans les récits d'antan, plane l'ombre majestueuse du Langskip, symbole de l'audace des Scandinaves. Contrairement à la croyance populaire, le fameux "drakkar" n'a jamais existé. Il s'agit en réalité du Langskip, popularisé par l'officier napoléonien Auguste Jal dans son ouvrage de 1848.</p>
+    <p>Le Langskip n'était pas qu'un simple moyen de transport, mais un outil de conquête. Mesurant jusqu'à 50 mètres de long pour 8 mètres de large, sa robustesse n'avait d'égal que son agilité.</p>
+    <p>Construit selon des méthodes ancestrales, le Langskip était constitué de planches superposées, renforcées par des rivets en fer. Cette technique, le bordage à clin, lui conférait une solidité à toute épreuve, défiant les tempêtes les plus féroces.</p>
+    <p>La voile, initialement négligée, devint rapidement un élément crucial de la navigation viking à bord du Langskip. Permettant d'exploiter les vents pour accélérer les déplacements, elle fut adoptée en complément des rameurs réguliers.</p>
+    <p>Doté d'une quille, innovation majeure, le Langskip pouvait naviguer avec stabilité et précision, même dans les eaux les plus agitées. Cette pièce de bois, fixée sous la coque, assurait une meilleure tenue de cap et facilitait les manœuvres.</p>
+    <p>À bord du Langskip, les Vikings ont tracé des routes commerciales à travers l'Europe, bravant les éléments pour découvrir de nouveaux horizons. Leur héritage perdure, le Langskip demeurant le témoin immuable de leur courage et de leur ténacité marins.</p>
 </div>
 <Footer />
 
@@ -85,9 +92,17 @@
 
     .article {
         text-align: justify;
-        width: 90%;
+        width: 70%;
         margin: 30px auto;
 
+    }
+
+    .article p {
+        margin: 10px 0;
+    }
+
+    .article h4 {
+        font-size: large;
     }
 
     @media (min-width: 768px) {
@@ -122,6 +137,14 @@
         .iframe {
             width: 960px;
             height: 540px;
+        }
+
+        .article p {
+            font-size: 1.2em;
+        }
+
+        .article h4 {
+            font-size: 1.6em;
         }
     }
 </style>
